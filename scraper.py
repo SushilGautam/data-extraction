c836540deb4f6ec67ac9a7ded5871dd509e0dffa
Sudip Adhikari <xudip12@gmail.com>
2021-05-26 20:25:45 +0545

    initial commit

---
+++ scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+# FIRST STEP -> REQUEST AND RESPONSE
+resp = requests.get("https://quotes.toscrape.com/")  # request and get response
+
+# 2ND STEP -> MAKE RESPONSE BODY IN PERFECT EXTRACTABLE FORM
+soup = BeautifulSoup(
+    resp.text, "lxml"
+)  # html.parser make raw_response body to perfect form
+
+# data = [1, 2, 3, 4]
+# data[0]
+# 3RD STEP -> EXTRACT SELECTED DATA
+output = soup.select("span.text")
+
+print(output[0].text)
+
